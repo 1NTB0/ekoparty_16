564f789b99e0ef2b12549589ab17a7ea75b4e3b1
Ren Ding <ren@ptcfi.gtisc.gatech.edu>
2016-10-31 14:21:47 -0400

    improved coding

--- reversing/250/se.py
+++ reversing/250/se.py
@@ -10,10 +10,12 @@ path_group.explore(find=get_flag_func, avoid=goodbye_func)
 
 # get all paths that're stored at the <found> stash
 for path in path_group.found:
-    #'''
     num_constraints = len(path.state.se.constraints)
-    for i in range(0, num_constraints):
-        print "input: ",
-        print repr(path.state.posix.dumps(i))
-    #'''
+    print "num constraints to solve in this path: " + str(num_constraints)
+    print "input: ",
+    print repr(path.state.posix.dumps(0))
+    print "output: ",
+    print repr(path.state.posix.dumps(1))
+    
+    # used when there's a specific filter in the results that you want to apply in angr's path state
     #print repr(path.state.se.any_str())
