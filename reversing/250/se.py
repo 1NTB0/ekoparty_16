46164b36e12199164fb5c7149e3596cc83f9ff85
Ren Ding <ren@ptcfi.gtisc.gatech.edu>
2016-10-31 14:06:58 -0400

    solved RE250 challenge

--- reversing/250/se.py
+++ reversing/250/se.py
@@ -1,14 +1,19 @@
-import angr, simuvex
+import angr
 
-b = angr.Project('./PFUck_binary', load_options={"auto_load_libs": False})
-state = b.factory.blank_state(addr=0x0400c05)
+get_flag_func = 0x403a23
+goodbye_func = 0x4051f1
 
-states = [state]
-while len(states) > 0:
-    successors = []
-    for state in states:
-        successors.extend(b.factory.sim_run(state).all_successors)
-    states = successors
-    print states
+b = angr.Project('./FUck_binary')
+state = b.factory.entry_state()
+path_group = b.factory.path_group(state)
+path_group.explore(find=get_flag_func, avoid=goodbye_func)
 
-print "Done"
+# get all paths that're stored at the <found> stash
+for path in path_group.found:
+    #'''
+    num_constraints = len(path.state.se.constraints)
+    for i in range(0, num_constraints):
+        print "input: ",
+        print repr(path.state.posix.dumps(i))
+    #'''
+    #print repr(path.state.se.any_str())
